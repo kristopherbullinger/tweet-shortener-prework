2baa72f8b5a3f900e1c5e27dc0d2f0a498218479
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-16 23:38:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def dictionary(word)
+  dictionary = {
+  'hello': 'hi',
+  'to': '2',
+  'two': '2',
+  'too': '2',
+  'for': '4',
+  'four': '4',
+  'you': 'u',
+  'at': '@',
+  'and': '&'
+  }
+  word = (dictionary.keys.include? word) ? dictionary[word.to_sym] : word
+end
+
+def word_substituter(long_tweet)
+  long_tweet = long_tweet.split(" ")
+  shorter_tweet = long_tweet.collect do |word|
+    dictionary(word.to_sym).to_s
+  end
+  shorter_tweet.join(" ")
+end
