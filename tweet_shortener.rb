93612c119e85c2f32d5d254c6f9b1a0f5d3a0b7f
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-16 23:42:35 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(long_tweet)
   long_tweet = long_tweet.split(" ")
   shorter_tweet = long_tweet.collect do |word|
-    dictionary(word.downcase.to_sym).to_s
+    dictionary(word.to_sym).to_s
   end
   shorter_tweet.join(" ")
 end
