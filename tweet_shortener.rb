9fa0696126bf26593f2bc343c63814954828d025
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-16 23:50:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,5 +29,5 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  puts word_substituter(tweet) if tweet.length > 130# && tweet.length 
+  word_substituter(tweet) if tweet.length > 130# && tweet.length 
 end
